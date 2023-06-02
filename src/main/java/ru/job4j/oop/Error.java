4d4a20e6564970d7d9d8c641e317c7adfd8cc7a6
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-06-03 00:51:01 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message = message;
+        this.status = status;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(message);
+        System.out.println(status);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 2, "error");
+        error.printInfo();
+        Error err = new Error();
+        err.printInfo();
+    }
+}
