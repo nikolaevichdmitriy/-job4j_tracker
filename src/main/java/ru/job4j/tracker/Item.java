69a7b196c1acbb585eb1df3607ac2a77f1ecbf6c
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-07-09 01:12:31 +0300

    8. Date. Отображение даты. [#310017  [#310017 #428435]]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,12 +4,11 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
