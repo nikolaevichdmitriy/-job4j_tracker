0094f2170fe1c802e0a10500e9454271bcc726ed
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2024-02-17 19:30:41 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,18 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\'' + ", created=" + created + '}';
+    }
+
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item(int id, String name) {
         this.id = id;
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item(12, "Mask");
         System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
-
+        Item item1 = new Item(10, "Say");
+        System.out.println(item1.toString());
     }
 }
