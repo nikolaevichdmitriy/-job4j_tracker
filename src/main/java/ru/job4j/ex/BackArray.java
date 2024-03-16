708b3576b52d5bf9d35f333c503ef67320098e33
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2024-03-16 20:34:27 +0300

    0. Что такое исключение. [#3821 #486264]

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -2,15 +2,11 @@ package ru.job4j.ex;
 
 public class BackArray {
     public String[] turn(String[] names) {
-        if (names == null) {
-            return null;
-        }
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
             String temp = names[index];
             names[index] = names[names.length - 1 - index];
             names[names.length - 1 - index] = temp;
-
         }
         return names;
     }
