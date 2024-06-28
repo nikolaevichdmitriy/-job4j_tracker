2cc3a11a3aaade22caa2bc5db85c2376a3f5eab4
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2024-06-28 20:33:13 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.lnput.Input;
+import ru.job4j.tracker.lnput.MockInput;
+import ru.job4j.tracker.lnput.ValidateInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[]{"one", "1"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInputPositive() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[]{"1"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInputMultiple() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[]{"1", "2", "6"});
+        ValidateInput input = new ValidateInput(output, in);
+        int[] expected = {1, 2, 6};
+        for (int i = 0; i < expected.length; i++) {
+            int selected = input.askInt("Enter menu:");
+            assertThat(selected).isEqualTo(expected[i]);
+        }
+    }
+
+    @Test
+    void whenInvalidInputNegative() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[]{"-1"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+}
