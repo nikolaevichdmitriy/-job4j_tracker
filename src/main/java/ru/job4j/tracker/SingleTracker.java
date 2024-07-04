a44d18242a53f4a7cecb2e62c17dec84b62c1656
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2024-07-04 16:13:22 +0300

    41. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private static SingleTracker instance;
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    private static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    /* Остальные методы из класса Tracker. */
+}
